d9ca9d5a74980b92090c44cbcf886d6e9b686fd7
Dario Kolic <coachbuilds@gmail.com>
2020-07-03 16:43:19 +0200

    Updated Animation on Enter Verification

--- js/enter.js
+++ js/enter.js
@@ -1,3 +1,24 @@
-let backBtn = document.querySelector('#bnt-no');
+let backBtn = document.querySelector('#btn-no');
+let animBtn = document.querySelector('#btn-yes');
 
-backBtn.addEventListener('click', () => window.history.back);
+let wrapperAnim = document.querySelector('.enter-wrapper');
+let verifyAnim = document.querySelector('.enter-verify');
+let imgAnimOpacity = document.querySelector('#img-bg');
+let letAnim = document.querySelector('#img-let');
+
+backBtn.addEventListener('click', () => {
+    window.history.back();
+});
+
+animBtn.addEventListener('click', () => {
+
+    setTimeout(() => {
+        location = 'main.html';
+    }, 1500);
+
+    verifyAnim.style.opacity = 0;
+    imgAnimOpacity.style.opacity = 0;
+    letAnim.style.top = '38%';
+    letAnim.style.width = '90%';
+
+});
