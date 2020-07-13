59dc792f9747173ca317bb50a1f2b00a1df97f4e
Dario Kolic <coachbuilds@gmail.com>
2020-07-13 18:35:31 +0200

    Update to shop!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
+let imgURL = '';
+let name = '';
+
+
 // Navigation - Hide on Scroll
 
 let lastScroll = 0;
 let currentScroll = 0;
-let navbar = document.querySelector('nav');
+const navbar = document.querySelector('nav');
 
-let shopCart = document.querySelector('.cart');
-let asideShop = document.querySelector('.shop-aside');
-let body = document.querySelector('body');
-let asideBack = document.querySelector('a.back');
+const shopCart = document.querySelector('.cart');
+const asideShop = document.querySelector('.shop-aside');
+const body = document.querySelector('body');
+const asideBack = document.querySelector('a.back');
+
 
 //Hide navbar
 
@@ -15,43 +20,72 @@ window.addEventListener('scroll', () => {
     currentScroll = window.pageYOffset;
     if(lastScroll > currentScroll){
         navbar.style.top = '0';
+        asideShop.style.top = '60px';
     }else{
         navbar.style.top = '-65.50px'
+        asideShop.style.top = '0px';
     }
     lastScroll = currentScroll;
 });
 
+// ********** SHOP ********** 
+
 // Shop aside functionality
 
-let addBtn = document.querySelector('.add-to-item');
-let subBtn = document.querySelector('.sub-to-item');
 
-let inputQuantity = document.querySelector('.shop-aside-q');
+const shopItems = [{
+
+}]
+
+
+
+
+const addBtn = document.querySelector('.add-to-item');
+const subBtn = document.querySelector('.sub-to-item');
+const para = document.querySelector('.shop-aside p');
+const orderBtn = document.querySelectorAll('.order');
+const quantityTemp = document.querySelector('.quantity')
+
+const inputQuantity = document.querySelector('.shop-aside-q');
+
+let storeAddInfo = '';
 
 let shopToggler = true;
 
 
+orderBtn.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        const orderParent = item.parentElement;
+        const orderChildArr = orderParent.children;
+        storeAddInfo = orderChildArr[0];
+        console.log(storeAddInfo.textContent);
+
+        if(shopToggler){
+            asideShop.style.left = "70%";
+            shopToggler = false;
+        }
+        let test = document.createElement('H1');
+        test.innerHTML = storeAddInfo.textContent;
+        asideShop.append(test);
+        asideShop.append(quantity);
+
+    });
+})
 
 shopCart.addEventListener('click', () => {
-
-
     if(shopToggler){
-        asideShop.style.left = '70%';
+        asideShop.style.left = "70%";
         shopToggler = false;
     }else{
-        asideShop.style.left = '100%';
+        asideShop.style.left = "100%";
         shopToggler = true;
     }
 });
 
-addBtn.addEventListener('mousedown', () => inputQuantity.value++);
 
-subBtn.addEventListener('mousedown', () => inputQuantity.value--);
 
-let shop = document.querySelector('.shop-wrapper');
+// addBtn.addEventListener('mousedown', () => inputQuantity.value++);
 
-console.log(shop);
+// subBtn.addEventListener('mousedown', () => inputQuantity.value--);
 
-let shopList = Array.from(shop.children);
 
-console.log(shopList[2])
